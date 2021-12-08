aca364abb6a825930f91ef4810c0d17316b92c79
Spencer Merrow <smerrow@disroot.org>
2021-12-07 22:37:24 -0500

    wip

--- anagrammar.py
+++ anagrammar.py
@@ -1,12 +1,9 @@
 from os import system, name
-from sys import setrecursionlimit
-
-setrecursionlimit(10**6)
 
 anagrams = []
 parts = []
 
-#clear = lambda: system('cls' if name=='nt' else '#clear')
+clear = lambda: system('cls' if name=='nt' else '#clear')
 
 def totalAnagram(source, check):
     if len(source) != len(check):
@@ -20,37 +17,25 @@ def totalAnagram(source, check):
     
     return True
 
-def recursiveFind(word, remain, index):
-    global parts
-    isPartial = True
-
-    try:
-        check = parts[index]
-    except IndexError:
-        return None
-
-    if len(check) > len(remain):
-            isPartial = False
-    else:
+def getNext(part, remaining):
+    for check in parts:
+        if len(check) > len(remaining):
+            continue
+        
+        isPartial = True
         for char in check:
-            if char not in remain:
+            if char not in remaining:
                 isPartial = False
                 break
-            if check.count(char) > remain.count(char):
+
+            if check.count(char) != remaining.count(char):
                 isPartial = False
                 break
+        
+        if isPartial:
+            return part.capitalize() + check.capitalize()
     
-    if isPartial:
-        word += check.capitalize()
-        print(word)
-        for char in check.lower():
-            remain = remain.replace(char, '')
-    
-    if remain:
-        recursiveFind(word, remain, (index+1))
-    else:
-        return word
-    
+    return None
 
             
 
@@ -72,8 +57,12 @@ with open('words.txt', 'r') as words:
             if char not in sourceWord:
                 partial = False
                 break
+
+            if line.count(char) > sourceWord.count(char):
+                partial = False
+                break
         
-        if partial:
+        if partial and len(line) > 1:
             parts.append(line)
         
         if len(line) != len(sourceWord):
@@ -82,12 +71,25 @@ with open('words.txt', 'r') as words:
         if totalAnagram(sourceWord, line):
             anagrams.append(line)
 
-print(len(parts))
+for p in parts: 
+    word = p
+    while True:
+        remaining = sourceWord
+        for char in word.lower():
+            remaining = remaining.replace(char, '')
+        
+        workingPartial = getNext(word.lower(), remaining)
+        if not workingPartial:
+            word = None
+            break
+        
+        word = workingPartial
 
-for i in range(len(parts)):
-    potentialAnagram = recursiveFind('', sourceWord, i)
-    if potentialAnagram and potentialAnagram not in anagrams:
-        anagrams.append(potentialAnagram)
+        if not remaining:
+            break
+    
+    if word and (word.lower() not in anagrams):
+        anagrams.append(word)
 
 #clear()
 print('Anagrams found:\n')
